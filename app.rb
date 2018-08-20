85ebbfa124b63a0d0f29e29a4a77c98960319df6
Ibteda Sabur <akkount35@gmail.com>
2018-08-20 14:35:10 -0400

    made a signup form for testing database 'transactions'

--- app.rb
+++ app.rb
@@ -11,4 +11,16 @@ get "/signup" do
   erb :signup
 end
 
+post "/signup" do
+  @email = params[:email]
+  @first_name = params[:first_name]
+  @last_name = params[:last_name]
+  @birthday = params[:birthday]
+
+  puts @email
+  puts @first_name
+  puts @last_name
+  puts @birthday
+end
+
 require "./models"
