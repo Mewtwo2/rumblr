15d0d59a029249c89a4c1e802afee76db450383d
Ibteda Sabur <akkount35@gmail.com>
2018-08-21 18:24:04 -0400

    added image to home page and other misc things

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_150718) do
+ActiveRecord::Schema.define(version: 2018_08_21_162215) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "text_content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-# db/seeds.rb
-users = [
-  {first_name: 'Jon', last_name: 'Doe', email: 'e@example.com'}
-]
-
-users.each do |u|
-  User.create(u)
-end
